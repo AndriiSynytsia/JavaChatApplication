efaa4a14f8f5e25eb83fcc7d2d761f1aa2e2f129
Andrii Synytsia <andreysinitsa4315@gmail.com>
2024-08-09 21:09:03 -0600

    Implemented ChatServer and ClientHandler. Server can accept client connections by creating new threads and broadcast messages to clients

---
+++ src/com/andriisynytsia/javachatapp/server/ChatServer.java
@@ -0,0 +1,27 @@
+package com.andriisynytsia.javachatapp.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServer {
+    // List to keep track of all connected clients
+    private static List<ClientHandler> clients = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(3000);
+        System.out.println("Server started. Waiting for clients...");
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket);
+
+            // Spawn a new thread for each client
+            ClientHandler clientThread = new ClientHandler(clientSocket, clients);
+            clients.add(clientThread);
+            new Thread(clientThread).start();
+        }
+    }
+}
